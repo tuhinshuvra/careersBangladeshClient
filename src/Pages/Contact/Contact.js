d578b6a978b29a70874427b468a021c43f25ac20
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-16 16:10:06 +0600

    added contact me for responsive bootstrap

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,48 +3,46 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 w1/2">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Name</span>
-                                </label>
-                                <input type="text" placeholder="Full Name" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Phone</span>
-                                </label>
-                                <input type="text" placeholder="phone" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Message</span>
-                                </label>
-                                <input type="textarea" rows="4" cols="50" className="input input-bordered" />
-                            </div>
-
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Submit</button>
-                            </div>
+            <div className="col-lg-12 px-4 py-5">
+                <div className="row align-items-center g-lg-5 py-5">
+                    <div className="col-lg-5 text-center text-lg-start">
+                        <div className="">
+                            <h1 className="text-3xl font-bold mb-5">Get in Touch!</h1>
+                            <p>Address: H-20, Shah Makdum Avenue, Uttara-11, Dhaka</p>
+                            <p>Email: abedefth@gmail.com</p>
+                            <p>WhatsApp +8801123456789</p>
+                            <p>Linkedin: @abcdefgh</p>
+                            <p>Facebook @abcdefghijk</p>
                         </div>
                     </div>
-                    <div className="text-center lg:text-left w-1/2">
-                        <h1 className="text-3xl font-bold mb-5">Get in Touch!</h1>
-                        <p>Address: H-20, Shah Makdum Avenue, Uttara-11, Dhaka</p>
-                        <p>Email: abedefth@gmail.com</p>
-                        <p>WhatsApp +8801123456789</p>
-                        <p>Linkedin: @abcdefgh</p>
-                        <p>Facebook @abcdefghijk</p>
+                    <div className="col-lg-6 mx-auto col-lg-5">
+                        <form className="p-4 p-md-5 border rounded-3 bg-light">
+                            <div className="form-floating mb-3">
+                                <input type="text" className="form-control" id="floatingInput" placeholder="Input full name" />
+                                <label for="floatingInput">Full Name</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                                <label for="floatingInput">Email address</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="text" className="form-control" id="floatingInput" placeholder="0123456789" />
+                                <label for="floatingInput">Phone No</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <textarea type="message" className="form-control" id="floatingInput" placeholder="Input message" />
+                                <label for="floatingInput">Message</label>
+                            </div>
+
+                            {/* <div className="checkbox mb-3">
+                                <label>
+                                    <input type="checkbox" value="remember-me" /> Remember me
+                                </label>
+                            </div> */}
+                            <button className="w-100 btn btn-lg btn-outline-primary" type="submit">Submit</button>
+                            {/* <hr className="my-4" /> */}
+                            {/* <small className="text-muted">By clicking Sign up, you agree to the terms of use.</small> */}
+                        </form>
                     </div>
                 </div>
             </div>
