7a5cc0fdcb7ab1dc1a07e22fd1ab73de68e6e400
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-16 10:59:46 +0600

    updated couter section in job search for responsice bootstrap

--- src/Pages/JobSearch/JobSearch.js
+++ src/Pages/JobSearch/JobSearch.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { FaFacebook, FaGithub, FaGoogle, FaTwitter } from 'react-icons/fa';
+import { faBuildingUser, faChair, faPerson } from '@fortawesome/free-solid-svg-icons'
 import './JobSearch.css';
 
 const JobSearch = () => {
-    const coffee = <FontAwesomeIcon icon={faCoffee} />
+    const company = <FontAwesomeIcon icon={faBuildingUser} />
+    const person = <FontAwesomeIcon icon={faPerson} />
+    const chair = <FontAwesomeIcon icon={faChair} />
+
     return (
         <div className='my-5'>
             <h2 className=' text-center fw-bold'>LETS FIND YOUR JOB</h2>
@@ -22,26 +24,23 @@ const JobSearch = () => {
 
                 <div class="row justify-content-center">
 
-                    <div class=" col-md-3 gap-2 ">
+                    <div class=" col-md-3  ">
                         <div class="counter-box ">
-                            <i class="fa fa-thumbs-o-up"></i>
-                            <i>{coffee} </i>
+                            <i>{chair} </i>
                             <span class="counter">21471</span>
                             <p>Live Jobs</p>
                         </div>
                     </div>
-                    <div class=" col-md-3 gap-2">
+                    <div class=" col-md-3 ">
                         <div class="counter-box ">
-                            <i class="fa fa-thumbs-o-up"></i>
-                            <i>{coffee} </i>
+                            <i>{company} </i>
                             <span class="counter">2343</span>
                             <p>Companies</p>
                         </div>
                     </div>
-                    <div class=" col-md-3 gap-2">
+                    <div class=" col-md-3 ">
                         <div class="counter-box ">
-                            <i class="fa fa-thumbs-o-up"></i>
-                            <i>{coffee} </i>
+                            <i>{person} </i>
                             <span class="counter">2147</span>
                             <p>Employers</p>
                         </div>
@@ -49,36 +48,6 @@ const JobSearch = () => {
 
                 </div>
             </div>
-
-
-
-
-            {/* <div className=" d-flex justify-content-center ">
-                <div className="stat">
-                    <div className="stat-figure text-secondary  hidden lg:block">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    </div>
-                    <h5 className="">Live Jobs</h5>
-                    <h2 className="stat-value">23452</h2>
-                </div>
-
-                <div className="stat">
-                    <div className="stat-figure text-secondary  hidden lg:block">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
-                    </div>
-                    <div className="stat-title text-2xl">Companies</div>
-                    <div className="stat-value">2154</div>
-                </div>
-
-                <div className="stat">
-                    <div className="stat-figure text-secondary  hidden lg:block">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
-                    </div>
-                    <div className="stat-title text-2xl">Employers</div>
-                    <div className="stat-value">1,207</div>
-                </div>
-
-            </div> */}
         </div>
     );
 };
