a519c0d87f0695f415c2bbcbb0850f35f1b80cb8
ttuhin Shuvra <tuhinshuvra18@gmail.com>
2023-03-06 13:35:08 +0600

    added some update

--- src/Pages/Banner/AnimatedBanner.js
+++ src/Pages/Banner/AnimatedBanner.js
@@ -49,6 +49,7 @@ const AnimatedBanner = () => {
                     </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide3" className="btn btn-circle">❮</a>
+                        
                         <a href="#slide1" className="btn btn-circle">❯</a>
                     </div>
                 </div>
