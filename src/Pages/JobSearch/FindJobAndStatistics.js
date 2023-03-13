4ebc74a7364e7003a5b261c185bc67b54746a07f
ttuhin Shuvra <tuhinshuvra18@gmail.com>
2023-03-13 12:48:56 +0600

    added password show option in login and register page

--- src/Pages/JobSearch/FindJobAndStatistics.js
+++ src/Pages/JobSearch/FindJobAndStatistics.js
@@ -1,160 +1,147 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { FaSearchDollar, FaChartLine, FaRegBuilding } from 'react-icons/fa';
-import { BsPeopleFill } from 'react-icons/bs';
-import './FindJobHome.css';
-import { Link } from 'react-router-dom';
-import CountUp from 'react-countup';
-import { AuthContext } from '../Authentication/AuthProvider';
-import './FindJobAndStatistics.css';
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { FaSearchDollar, FaChartLine, FaRegBuilding } from "react-icons/fa";
+import { BsPeopleFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import CountUp from "react-countup";
+import { AuthContext } from "../Authentication/AuthProvider";
+import "./FindJobHome.css";
 
 const FindJobAndStatistics = () => {
+  const [totalJobs, setTotalJobs] = useState("1");
+  const [totalCompanies, setTotalCompanies] = useState("1");
+  const [totalEmployers, setTotalEmployers] = useState("1");
 
-    const [totalJobs, setTotalJobs] = useState('1');
-    const [totalCompanies, setTotalCompanies] = useState('1');
-    const [totalEmployers, setTotalEmployers] = useState('1');
+  const { setSearchData } = useContext(AuthContext);
 
+  // const searchRef = useRef();
 
-    const { setSearchData } = useContext(AuthContext);
+  useEffect(() => {
+    fetch("http://localhost:5000/jobs")
+      .then((response) => response.json())
+      .then((data) => setTotalJobs(data));
+  }, []);
 
-    // const searchRef = useRef();
+  useEffect(() => {
+    fetch("http://localhost:5000/employers")
+      .then((response) => response.json())
+      .then((data) => setTotalEmployers(data));
+  }, []);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/jobs')
-            .then(response => response.json())
-            .then(data => setTotalJobs(data))
-    }, [])
+  const totalJobNo = totalJobs.length + 10000;
+  const totalCompanyNo = totalEmployers.length + 1000;
+  const totalEmployersNo = totalEmployers.length + 2000;
+  console.log("totalJobs : ", totalJobNo);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/employers')
-            .then(response => response.json())
-            .then(data => setTotalEmployers(data))
-    }, [])
+  // useEffect(() => {
+  //     fetch(`http://localhost:5000/jobSearch?search=${search}`)
+  //         .then(response => response.json())
+  //         .then(data => {
+  //             console.log("Find Job Home Search Result :", data)
+  //         })
+  // }, [search]);
 
+  const getSearchKeyword = (event) => {
+    const jobTitle = event.target.value;
+    setSearchData(jobTitle);
+  };
 
-    const totalJobNo = totalJobs.length + 10000;
-    const totalCompanyNo = totalEmployers.length + 1000;
-    const totalEmployersNo = totalEmployers.length + 2000;
-    console.log("totalJobs : ", totalJobNo);
+  const getSearchOrganization = (event) => {
+    const orgaName = event.target.value;
+    setSearchData(orgaName);
+  };
 
+  const getSearchPlace = (event) => {
+    const placeName = event.target.value;
+    setSearchData(placeName);
+  };
 
-    // useEffect(() => {
-    //     fetch(`http://localhost:5000/jobSearch?search=${search}`)
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             console.log("Find Job Home Search Result :", data)
-    //         })
-    // }, [search]);
+  return (
+    <div className="findAndStat">
+      <div className="col-lg-10 mx-auto ">
+        <h2 className="careers_title_one">
+          <FaSearchDollar className="mx-1"></FaSearchDollar>LETS FIND YOUR JOB
+        </h2>
+        <div className="d-flex  justify-content-center mt-4">
+          <input
+            onBlur={getSearchKeyword}
+            name="jobTitle"
+            type="text"
+            id="jobTitle"
+            placeholder="Search with keyword"
+            className="form-control  w-25"
+          />
 
+          <select
+            onBlur={getSearchOrganization}
+            name="orgaType"
+            className="form-select w-25 mx-2"
+          >
+            <option>Select Company Type</option>
+            <option value="Government">Government</option>
+            <option value="Semi Government">Semi Government</option>
+            <option value="NGO">NGO</option>
+            <option value="Private Firm">Private Firm</option>
+            <option value="International Agencies">
+              International Agencies
+            </option>
+            <option value="Others">Others</option>
+          </select>
 
+          <input
+            onBlur={getSearchPlace}
+            name="location"
+            type="text"
+            id="location"
+            placeholder="Location"
+            className="form-control  w-25"
+          />
 
-    const getSearchKeyword = event => {
-        const jobTitle = event.target.value;
-        setSearchData(jobTitle);
-    }
-
-    const getSearchOrganization = event => {
-        const orgaName = event.target.value;
-        setSearchData(orgaName);
-    }
-
-    const getSearchPlace = event => {
-        const placeName = event.target.value;
-        setSearchData(placeName);
-    }
-
-    return (
-        <div className='findAndStat'>
-            <div className='col-lg-10 mx-auto '>
-                <h2 className='careers_title_one'><FaSearchDollar className='mx-1'></FaSearchDollar>LETS FIND YOUR JOB</h2>
-                <div className="d-flex  justify-content-center mt-4">
-                    <input
-                        onBlur={getSearchKeyword}
-                        name="jobTitle"
-                        type="text"
-                        id="jobTitle"
-                        placeholder="Search with keyword"
-                        className="form-control  w-25"
-                    />
-
-                    <select
-                        onBlur={getSearchOrganization}
-                        name='orgaType'
-                        className="form-select w-25 mx-2">
-                        <option>Select Company Type</option>
-                        <option value="Government">Government</option>
-                        <option value="Semi Government">Semi Government</option>
-                        <option value="NGO">NGO</option>
-                        <option value="Private Firm">Private Firm</option>
-                        <option value="International Agencies">International Agencies</option>
-                        <option value="Others">Others</option>
-                    </select>
-
-
-                    <input
-                        onBlur={getSearchPlace}
-                        name="location"
-                        type="text"
-                        id="location"
-                        placeholder="Location"
-                        className="form-control  w-25"
-                    />
-
-
-                    <Link to={`/searchHomeResult`} className=' text-decoration-none custom_btn ms-2' > Search</Link>
-
-                </div>
-
-                <div className=' my-5'>
-
-                    <div className="row justify-content-center">
-                        <div className=" col-md-3  home_stat ">
-                            <div className="counter-box glow-on-hover ">
-                                <FaChartLine className='  text-secondary fs-1 mt-1'></FaChartLine>
-                                <span className="counter">
-                                    <CountUp
-                                        start={1}
-                                        end={totalJobNo}
-                                        duration={5}
-                                    />
-                                </span>
-
-                                <p>Live Jobs</p>
-                            </div>
-                        </div>
-
-                        <div className=" col-md-3 ">
-                            <div className="counter-box  glow-on-hover">
-                                <FaRegBuilding className='  text-secondary fs-1 mt-1'></FaRegBuilding>
-                                <span className="counter">
-                                    <CountUp
-                                        start={1}
-                                        end={totalCompanyNo}
-                                        duration={5}
-                                    />
-                                </span>
-                                <p className=''>Companies</p>
-                            </div>
-                        </div>
-
-                        <div className=" col-md-3 ">
-                            <div className="counter-box glow-on-hover ">
-                                <BsPeopleFill className='  text-secondary fs-1 mt-1'></BsPeopleFill>
-                                <span className="counter">
-                                    <CountUp
-                                        start={1}
-                                        end={totalEmployersNo}
-                                        duration={5}
-                                    />
-                                </span>
-                                <p>Employers</p>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+          <Link
+            to={`/searchHomeResult`}
+            className=" text-decoration-none custom_btn ms-2"
+          >
+            {" "}
+            Search
+          </Link>
         </div>
-    );
+
+        <div className=" my-5">
+          <div className="row justify-content-center">
+            <div className=" col-md-3  home_stat ">
+              <div className="counter-box glowOnHover">
+                <FaChartLine className="  text-secondary fs-1 mt-1"></FaChartLine>
+                <span className="counter">
+                  <CountUp start={1} end={totalJobNo} duration={5} />
+                </span>
+
+                <p>Live Jobs</p>
+              </div>
+            </div>
+
+            <div className=" col-md-3 ">
+              <div className="counter-box  glowOnHover">
+                <FaRegBuilding className="  text-secondary fs-1 mt-1"></FaRegBuilding>
+                <span className="counter">
+                  <CountUp start={1} end={totalCompanyNo} duration={5} />
+                </span>
+                <p className="">Companies</p>
+              </div>
+            </div>
+
+            <div className=" col-md-3 ">
+              <div className="counter-box glowOnHover">
+                <BsPeopleFill className="  text-secondary fs-1 mt-1"></BsPeopleFill>
+                <span className="counter">
+                  <CountUp start={1} end={totalEmployersNo} duration={5} />
+                </span>
+                <p>Employers</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FindJobAndStatistics;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,140 +1,211 @@
-import React, { useContext, useState } from 'react';
-import LoginLogo from '../../assets/logo/carriers-bangladesh-logo.png';
-import { FaFacebook, FaGithub, FaGoogle, FaTwitter } from 'react-icons/fa';
-import { Link, useNavigate } from 'react-router-dom';
-import { toast } from 'react-hot-toast';
-import { useForm } from 'react-hook-form';
-import { AuthContext } from '../Authentication/AuthProvider';
-import useTitle from '../../hooks/useTitle';
-import './Login.css';
+import React, { useContext, useState } from "react";
+import LoginLogo from "../../assets/logo/carriers-bangladesh-logo.png";
+import { FaFacebook, FaGithub, FaGoogle, FaTwitter } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../Authentication/AuthProvider";
+import useTitle from "../../hooks/useTitle";
+import "./Login.css";
 
 const Login = () => {
-    useTitle('Login');
-    const { signIn } = useContext(AuthContext);
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const [loginError, setLoginError] = useState('');
-    const [loginUserEmail, setLoginUserEmail] = useState('');
+  useTitle("Login");
+  const { signIn } = useContext(AuthContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [loginError, setLoginError] = useState("");
+  const [loginUserEmail, setLoginUserEmail] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLogin = (data) => {
-        setLoginError('');
-        signIn(data.email, data.password)
-            .then(result => {
-                const user = result.user;
-                toast.success('User Login Successfully.')
-                navigate("/");
-                // console.log(user);
-                setLoginUserEmail(data.email);
-            })
-            .catch(error => {
-                setLoginError(error.message);
-                toast.error(error.message);
-                // console.log(error);
-            })
-        // console.log(data);
+  const handleLogin = (data) => {
+    setLoginError("");
+    signIn(data.email, data.password)
+      .then((result) => {
+        const user = result.user;
+        toast.success("User Login Successfully.");
+        navigate("/");
+        // console.log(user);
+        setLoginUserEmail(data.email);
+      })
+      .catch((error) => {
+        setLoginError(error.message);
+        toast.error(error.message);
+        // console.log(error);
+      });
+    // console.log(data);
+  };
+
+  const handleShowPassword = () => {
+    let x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
     }
+  };
 
-    return (
-        <div>
-            <section className="gradient-form" style={{}}>
-                <div className="container py-5">
-                    <div className="row d-flex justify-content-center align-items-center">
-                        <div className="col-xl-10">
-                            <div className="card text-black">
-                                <div className="row g-0  rounded-5">
-                                    <div className="col-lg-5 d-flex flex-column justify-content-center align-items-center logo-area" style={{}}>
-                                        <div className="px-3 py-lg-4 p-md-5  mx-auto">
-                                            <img className='login_logo' src={LoginLogo} alt="" />
-                                        </div>
-                                        <h2 className=" mb-lg-4 text-white">Careers Bangladesh</h2>
-                                    </div>
-                                    <div className="col-lg-7">
-                                        <div className="card-body login_form p-5" style={{ backgroundColor: "#EEF1F6" }}>
-
-
-                                            <form onSubmit={handleSubmit(handleLogin)} className=' rounded rounded-2 bg-sm p-5 '>
-                                                {loginError && <p className=' font-bold text-red-600'>{loginError}</p>}
-                                                <div className=''>
-                                                    <h5 className=' mb-4 text-center fw-bold'>Please login to your account</h5>
-
-
-                                                    <div className="form-outline mb-4">
-                                                        <label className="form-label" htmlFor="userType">Select Role</label>
-                                                        <select type="text"
-                                                            {...register("userType")}
-                                                            className="form-control">
-                                                            <option value="employer">Employer</option>
-                                                            <option value="jobseeker" selected>Job Seeker</option>
-                                                        </select>
-                                                    </div>
-
-
-
-                                                    <div className="form-outline mb-4">
-                                                        <label className="form-label" htmlFor="username">Username</label>
-                                                        <input type="email" id="username"
-                                                            {...register("email", { required: "Email address is required" })}
-                                                            className="form-control"
-                                                            placeholder="Phone number or email address" />
-                                                    </div>
-
-                                                    <div className="form-outline mb-4">
-                                                        <label className="form-label" htmlFor="password">Password</label>
-                                                        <input type="password" id="password"
-                                                            {...register("password",
-                                                                {
-                                                                    required: "Password is required",
-                                                                    minLength: { value: 6, message: 'Password must be minimum 6 characters' }
-                                                                })}
-                                                            className="form-control" placeholder='Enter password' />
-                                                    </div>
-                                                    {errors.password && <p className=' text-red-600' role="alert">{errors.password?.message}</p>}
-
-                                                    <div className="text-center pt-1 mb-5 pb-1">
-                                                        <button className="custom_btn mb-3" type="submit" value='login'>
-                                                            Log In
-                                                        </button>
-                                                        <Link className="text-muted m-2 text-decoration-none " to="#">Forgot password?</Link>
-                                                    </div>
-
-                                                    <div className="d-flex align-items-center justify-content-center pb-4">
-                                                        <p className="mb-0 me-2">Don't have an account?</p>
-                                                        <Link to="/register"><button type="button" className=" custom_btn">Create new</button></Link>
-                                                    </div>
-
-                                                    <div className="text-center">
-                                                        <p>or sign up with:</p>
-                                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                                            <FaFacebook />
-                                                        </button>
-
-                                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                                            <FaGoogle />
-                                                        </button>
-
-                                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                                            <FaTwitter />
-                                                        </button>
-
-                                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                                            <FaGithub />
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </form>
-
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
+  return (
+    <div>
+      <section className="gradient-form" style={{}}>
+        <div className="container py-5">
+          <div className="row d-flex justify-content-center align-items-center">
+            <div className="col-xl-10">
+              <div className="card text-black">
+                <div className="row g-0  rounded-5">
+                  <div
+                    className="col-lg-5 d-flex flex-column justify-content-center align-items-center logo-area"
+                    style={{}}
+                  >
+                    <div className="px-3 py-lg-4 p-md-5  mx-auto">
+                      <img className="login_logo" src={LoginLogo} alt="" />
                     </div>
+                    <h2 className=" mb-lg-4 text-white">Careers Bangladesh</h2>
+                  </div>
+                  <div className="col-lg-7">
+                    <div
+                      className="card-body login_form p-5"
+                      style={{ backgroundColor: "#EEF1F6" }}
+                    >
+                      <form
+                        onSubmit={handleSubmit(handleLogin)}
+                        className=" rounded rounded-2 bg-sm p-5 "
+                      >
+                        {loginError && (
+                          <p className=" font-bold text-red-600">
+                            {loginError}
+                          </p>
+                        )}
+                        <div className="">
+                          <h5 className=" mb-4 text-center fw-bold">
+                            Please login to your account
+                          </h5>
+                          <div className="form-outline mb-4">
+                            <label className="form-label" htmlFor="userType">
+                              Select Role
+                            </label>
+                            <select
+                              type="text"
+                              {...register("userType")}
+                              className="form-control"
+                            >
+                              <option value="employer">Employer</option>
+                              <option value="jobseeker" selected>
+                                Job Seeker
+                              </option>
+                            </select>
+                          </div>
+                          <div className="form-outline mb-4">
+                            <label className="form-label" htmlFor="username">
+                              Username
+                            </label>
+                            <input
+                              type="email"
+                              id="username"
+                              {...register("email", {
+                                required: "Email address is required",
+                              })}
+                              className="form-control"
+                              placeholder="Phone number or email address"
+                            />
+                          </div>
+                          <div className="form-outline mb-4">
+                            <label className="form-label" htmlFor="password">
+                              Password
+                            </label>
+                            <input
+                              type="password"
+                              id="password"
+                              {...register("password", {
+                                required: "Password is required",
+                                minLength: {
+                                  value: 6,
+                                  message:
+                                    "Password must be minimum 6 characters",
+                                },
+                              })}
+                              className="form-control"
+                              placeholder="Enter password"
+                            />
+                          </div>
+
+                          {/* show password */}
+                          <input type="checkbox" onClick={handleShowPassword} />
+                          <span className=" ms-2"> Show Password</span>
+
+                          {errors.password && (
+                            <p className=" text-red-600" role="alert">
+                              {errors.password?.message}
+                            </p>
+                          )}
+                          <div className="text-center pt-1 mb-5 pb-1">
+                            <button
+                              className="custom_btn mb-3"
+                              type="submit"
+                              value="login"
+                            >
+                              Log In
+                            </button>
+                            <Link
+                              className="text-muted m-2 text-decoration-none "
+                              to="#"
+                            >
+                              Forgot password?
+                            </Link>
+                          </div>
+                          <div className="d-flex align-items-center justify-content-center pb-4">
+                            <p className="mb-0 me-2">Don't have an account?</p>
+                            <Link to="/register">
+                              <button type="button" className=" custom_btn">
+                                Create new
+                              </button>
+                            </Link>
+                          </div>
+                          <div className="text-center">
+                            <p>or sign up with:</p>
+                            <button
+                              type="button"
+                              className="btn btn-link btn-floating mx-1"
+                            >
+                              <FaFacebook />
+                            </button>
+
+                            <button
+                              type="button"
+                              className="btn btn-link btn-floating mx-1"
+                            >
+                              <FaGoogle />
+                            </button>
+
+                            <button
+                              type="button"
+                              className="btn btn-link btn-floating mx-1"
+                            >
+                              <FaTwitter />
+                            </button>
+
+                            <button
+                              type="button"
+                              className="btn btn-link btn-floating mx-1"
+                            >
+                              <FaGithub />
+                            </button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
                 </div>
-            </section>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Login;
